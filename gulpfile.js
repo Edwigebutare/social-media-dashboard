84921cb5a24594f2c2c13f75e1c22093670ca3ab
Chance <butarechanceedwige@gmail.com>
2024-04-26 15:57:25 +0200

    color modification

--- gulpfile.js
+++ gulpfile.js
@@ -58,3 +58,6 @@ function jsTask() {
 
     // Default Gulp Task
     exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+
+    //Build Gulp Task
+    exports.build = series(scssTask, jsTask);
